d57568490a504e59ef3628b4704514ff4cb4caff
Jesse Spangenberger <flamusdiu@gmail.com>
2015-05-22 22:21:14 -0400

    Adding missing js file

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	$('li.function').addClass('plusimageapply');
+	$('li.function').children().addClass('selectedimage');
+	$('li.function').children().hide();
+	$('span.brace').show();
+	$('li.function').each(function(column) {
+	  $(this).click(function(event) {
+		if (this == event.target) {
+		  if($(this).is('.plusimageapply')) {
+			$(this).children().show();
+			$(this).find('span.brace').hide();
+			$(this).find('.plusimageapply span.brace').show();
+			$(this).removeClass('plusimageapply');
+			$(this).addClass('minusimageapply');
+		  } else {
+			$(this).children().hide();
+			$(this).find('span.brace').show();
+			$(this).find('span.function-inner').show();
+			$(this).removeClass('minusimageapply');
+			$(this).addClass('plusimageapply');
+		  };
+		};
+	  });
+	 });
+});
